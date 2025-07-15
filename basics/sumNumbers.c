c657013e116b5a8e825e159bd1bd9de4a231248c
random <random@victor-Bug.technicolor.net>
2025-07-15 09:36:03 +0200

    First commit returning to C

---
+++ basics/sumNumbers.c
@@ -0,0 +1,34 @@
+// Ask the user to enter N, then input N integers and calculate their sum.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int length = 0;
+    int *numberArray = NULL;
+    int sum = 0;
+
+    printf("Enter the number of values you want to calculate: \n");
+    scanf("%d", &length);
+
+    // Dynamically allocate memory
+    numberArray = malloc(length * sizeof(int));
+
+    // Need to check whether memory worked
+    if (numberArray == NULL)
+    {
+        printf("Memory allocation failed");
+        return 1;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("\nInput a number: ");
+        scanf("%d", &numberArray[i]);
+        sum += numberArray[i];
+    }
+
+    printf("Total is: %d\n", sum);
+
+    return 0;
+}
